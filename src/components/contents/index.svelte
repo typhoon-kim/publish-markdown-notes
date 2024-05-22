cccaa6e136fa463dff06116d23d2faed0a51bfea
typhoon.kim <typh007.kim@gmail.com>
2024-05-22 20:29:12 +0900

    인덱스 컴포넌트 생성

---
+++ src/components/contents/index.svelte
@@ -0,0 +1,21 @@
+<script>
+    import ScrollArea from "$lib/components/ui/scroll-area/scroll-area.svelte";
+    import Separator from "$lib/components/ui/separator/separator.svelte";
+    import IndexItem from "./index_item.svelte";
+
+    export let title;
+    export let noteList;
+</script>
+<article class="border rounded-md shadow-md note note-single flex flex-col">
+    <section class="container flex flex-col">
+        <h1 class="text-3xl h-16 font-extrabold w-full grow flex justify-center place-items-center">
+            <span>{title}</span>
+        </h1>
+        <Separator class="my-2" />
+    </section>
+    <ScrollArea class="grow w-full">
+        {#each noteList as note}
+            <IndexItem {note} />
+        {/each}
+    </ScrollArea>
+</article>
---
+++ src/components/contents/index_item.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    import { CardHeader, Card, CardTitle, CardDescription, CardContent, CardFooter } from "$lib/components/ui/card";
+    import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "$lib/components/ui/breadcrumb";
+    import Badge from "$lib/components/ui/badge/badge.svelte";
+    import Separator from "$lib/components/ui/separator/separator.svelte";
+    import { Tags } from "lucide-svelte";
+
+    export let note;
+
+    const breadcrumb = note.route.split("/");
+    const title = note.title.replace(".md", "");
+</script>
+
+<Card class="grow mx-10 my-6 py-4">
+    <CardHeader class="py-0">
+        <CardDescription class="flex w-full">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    {#each breadcrumb as item}
+                        {#if item}
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href={item}>{item}</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                        {/if}
+                    {/each}
+                </BreadcrumbList>
+            </Breadcrumb>
+            <p class="grow text-end">
+                {`${new Date(note.created).toLocaleString()}`}
+            </p>
+        </CardDescription>
+        <Separator />
+        <CardTitle class="text-2xl border-s-4 ps-2">
+            <a href="#none">{title}</a>
+        </CardTitle>
+    </CardHeader>
+    <CardContent class="flex py-2 place-items-center mt-2">
+        <div class=" flex flex-col place-content-center pe-4 grow">
+            <div class="grow">
+                {note.summary}
+            </div>
+        </div>
+        {#if note.thumbnail && note.thumbnail !== "DEFAULT"}
+            <img class="w-36 ps-4 shrink" src={note.thumbnail} alt="thumbnail">
+        {/if}
+    </CardContent>
+    {#if note.tags && note.tags.length > 0}
+    <CardFooter>
+        <ul class="flex place-self-start">
+            <Tags class="me-1" />
+            {#each note.tags as tag}
+                <li class="mx-0.5"><Badge>{tag}</Badge></li>
+            {/each}
+        </ul>
+    </CardFooter>
+    {/if}
+</Card>
