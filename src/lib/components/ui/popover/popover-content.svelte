ad5d123d6949509db269da117149abac45bb33d0
typhoon.kim <typh007.kim@gmail.com>
2024-05-25 21:18:43 +0900

    add popover ui component

---
+++ src/lib/components/ui/popover/popover-content.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { Popover as PopoverPrimitive } from "bits-ui";
+	import { cn, flyAndScale } from "$lib/utils.js";
+	let className = undefined;
+	export let transition = flyAndScale;
+	export let transitionConfig = undefined;
+	export { className as class };
+</script>
+
+<PopoverPrimitive.Content
+	{transition}
+	{transitionConfig}
+	class={cn(
+		"z-50 w-fit rounded-md border bg-popover p-4 ms-4 text-popover-foreground shadow-md outline-none",
+		className
+	)}
+	{...$$restProps}
+	side="right-start"
+>
+	<slot />
+</PopoverPrimitive.Content>
