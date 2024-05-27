2c3ae8cfe542667cf41157ae3b62690c233757cc
typhoon.kim <typh007.kim@gmail.com>
2024-05-28 05:13:58 +0900

    마크다운 id 추가, 라우트 url 변경

--- src/components/contents/Markdown.svelte
+++ src/components/contents/Markdown.svelte
@@ -6,48 +6,55 @@
     let className = undefined;
     export { className as class };
 
-    $: breadcrumb = [];
-    $: title = "";
-
-    if (note) {
-         breadcrumb = note.route;
-         title = note.name;
-    }
 </script>
 
 <ScrollAreaWithProgress class={`h-full w-full ${className}`}>
     {#if note}
         <Breadcrumb class="px-4">
             <BreadcrumbList>
-                {#each breadcrumb as item}
+                {#each note.route as item, idx}
+                    {#if item}
                     <BreadcrumbItem>
-                        <BreadcrumbLink href={item}>{item}</BreadcrumbLink>
+                        <BreadcrumbLink 
+                            href={`/#/index?category=${note.route.slice(0, idx + 1).join("/")}`}
+                            >
+                            {item}
+                        </BreadcrumbLink>
                     </BreadcrumbItem>
                     <BreadcrumbSeparator />
+                    {/if}
                 {/each}
                 <BreadcrumbItem>
-                    <BreadcrumbPage>{title}</BreadcrumbPage>
+                    <BreadcrumbPage>{note.name}</BreadcrumbPage>
                 </BreadcrumbItem>
             </BreadcrumbList>
         </Breadcrumb>
-        <h1 class="text-4xl my-2 font-extrabold p-2">{title}</h1>
-        <zero-md
+        <h1 class="text-4xl my-1 font-extrabold p-2">{note.name}</h1>
+        <zero-md id={note.id}
             class="markdown-body w-full"
             src={`publish/${note.route.join("/")}/${note.name}.md`}>
             <script type="text/markdown">
                 > [!CAUTION]
                 > 😱 문서를 찾을 수 없습니다.
             </script>
+            <template data-prepend>
+                <style>
+                    img {
+                        max-width: 100%;
+                    }
+                </style>
+            </template>
             <template>
+                <style>
+                    .markdown-body {
+                        background-color: #00000000 !important;
+                        transition: all 0.3s ease-in-out;
+                    }
+                </style>
                 <link
                     rel="stylesheet"
                     href={`/node_modules/github-markdown-css/github-markdown-${$mode}.css`}
                 />
-                <style>
-                    .markdown-body {
-                        background-color: #00000000 !important;
-                    }
-                </style>
             </template>
         </zero-md>
     {:else}
@@ -57,15 +64,16 @@
                 > 😱 문서를 찾을 수 없습니다.
             </script>
             <template>
+                <style>
+                    .markdown-body {
+                        background-color: #00000000 !important;
+                        transition: all 0.3s ease-in-out;
+                    }
+                </style>
                 <link
                     rel="stylesheet"
                     href={`/node_modules/github-markdown-css/github-markdown-${$mode}.css`}
                 />
-                <style>
-                    .markdown-body {
-                        background-color: #00000000 !important;
-                    }
-                </style>
             </template>
         </zero-md>
     {/if}
