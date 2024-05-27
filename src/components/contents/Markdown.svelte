fba9d2e580f8ad8c24f938ef3d55c535d38c12b6
typhoon.kim <typh007.kim@gmail.com>
2024-05-27 18:23:04 +0900

    노트 폴백

--- src/components/contents/Markdown.svelte
+++ src/components/contents/Markdown.svelte
@@ -3,40 +3,70 @@
     import ScrollAreaWithProgress from "$lib/components/ui/scroll-area-with-progress/scroll-area-with-progress.svelte";
     import { mode } from "mode-watcher";
     export let note;
-	let className = undefined;
-	export { className as class };
+    let className = undefined;
+    export { className as class };
 
-    $: breadcrumb = note.route;
-    $: title = note.name;
+    $: breadcrumb = [];
+    $: title = "";
+
+    if (note) {
+         breadcrumb = note.route;
+         title = note.name;
+    }
 </script>
 
 <ScrollAreaWithProgress class={`h-full w-full ${className}`}>
-    <Breadcrumb class="px-4">
-        <BreadcrumbList>
-            {#each breadcrumb as item}
+    {#if note}
+        <Breadcrumb class="px-4">
+            <BreadcrumbList>
+                {#each breadcrumb as item}
+                    <BreadcrumbItem>
+                        <BreadcrumbLink href={item}>{item}</BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                {/each}
                 <BreadcrumbItem>
-                    <BreadcrumbLink href={item}>{item}</BreadcrumbLink>
+                    <BreadcrumbPage>{title}</BreadcrumbPage>
                 </BreadcrumbItem>
-                <BreadcrumbSeparator />
-            {/each}
-            <BreadcrumbItem>
-                <BreadcrumbPage>{title}</BreadcrumbPage>
-            </BreadcrumbItem>
-        </BreadcrumbList>
-    </Breadcrumb>
-    <h1 class="text-4xl my-2 font-extrabold p-2">{title}</h1>
-    <zero-md class="markdown-body w-full" src={`publish/${note.route.join("/")}/${note.name}.md`}>
-        <script type="text/markdown">
-            > [!CAUTION]
-            > 😱 문서를 찾을 수 없습니다.
-        </script>
-        <template>
-            <link rel="stylesheet" href={`/node_modules/github-markdown-css/github-markdown-${$mode}.css`} />
-            <style>
-                .markdown-body {
-                    background-color: #00000000 !important;
-                }
-            </style>
-        </template>
-    </zero-md>
+            </BreadcrumbList>
+        </Breadcrumb>
+        <h1 class="text-4xl my-2 font-extrabold p-2">{title}</h1>
+        <zero-md
+            class="markdown-body w-full"
+            src={`publish/${note.route.join("/")}/${note.name}.md`}>
+            <script type="text/markdown">
+                > [!CAUTION]
+                > 😱 문서를 찾을 수 없습니다.
+            </script>
+            <template>
+                <link
+                    rel="stylesheet"
+                    href={`/node_modules/github-markdown-css/github-markdown-${$mode}.css`}
+                />
+                <style>
+                    .markdown-body {
+                        background-color: #00000000 !important;
+                    }
+                </style>
+            </template>
+        </zero-md>
+    {:else}
+        <zero-md class="markdown-body w-full">
+            <script type="text/markdown">
+                > [!CAUTION]
+                > 😱 문서를 찾을 수 없습니다.
+            </script>
+            <template>
+                <link
+                    rel="stylesheet"
+                    href={`/node_modules/github-markdown-css/github-markdown-${$mode}.css`}
+                />
+                <style>
+                    .markdown-body {
+                        background-color: #00000000 !important;
+                    }
+                </style>
+            </template>
+        </zero-md>
+    {/if}
 </ScrollAreaWithProgress>
